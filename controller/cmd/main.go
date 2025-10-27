c502483e7beff000bcb754cc14eb311cad643921
Loutres <me@loutres.me>
2025-10-27 12:53:09 +0900

    c

--- controller/cmd/main.go
+++ controller/cmd/main.go
@@ -245,6 +245,13 @@ func main() {
 			os.Exit(1)
 		}
 	}
+	if err = (&controller.SubnetLeaseReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "SubnetLease")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	if metricsCertWatcher != nil {
