ef3b12fd784ece10cb6feaff505b9650ec22f537
Loutres <me@loutres.me>
2025-12-30 22:57:24 +0900

    add: api definitions

---
+++ controller/api/v1alpha1/bgpadvertisement_types.go
@@ -0,0 +1,55 @@
+/*
+Copyright 2025.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// BGPAdvertisementSpec defines the desired state of BGPAdvertisement.
+type BGPAdvertisementSpec struct {
+	AddressPools []string `json:"addressPools,omitempty"`
+}
+
+// BGPAdvertisementStatus defines the observed state of BGPAdvertisement.
+type BGPAdvertisementStatus struct {
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+
+// BGPAdvertisement is the Schema for the bgpadvertisements API.
+type BGPAdvertisement struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   BGPAdvertisementSpec   `json:"spec,omitempty"`
+	Status BGPAdvertisementStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// BGPAdvertisementList contains a list of BGPAdvertisement.
+type BGPAdvertisementList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []BGPAdvertisement `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&BGPAdvertisement{}, &BGPAdvertisementList{})
+}
