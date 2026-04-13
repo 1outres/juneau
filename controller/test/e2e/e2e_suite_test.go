0511e89ba8a98d1a976aee2bbeeb906943f449df
loutres <github@loutres.me>
2026-04-13 10:08:40 +0900

    test: add webhook cert job image

--- controller/test/e2e/e2e_suite_test.go
+++ controller/test/e2e/e2e_suite_test.go
@@ -40,6 +40,8 @@ var (
 	// projectImage is the name of the image which will be build and loaded
 	// with the code source changes to be tested.
 	projectImage = "example.com/controller:v0.0.1"
+	// webhookCertJobImage is the in-cluster image used by the bootstrap/rotation jobs.
+	webhookCertJobImage = "example.com/webhookcertjob:v0.0.1"
 )
 
 // TestE2E runs the end-to-end (e2e) test suite for the project. These tests execute in an isolated,
@@ -58,11 +60,22 @@ var _ = BeforeSuite(func() {
 	_, err := utils.Run(cmd)
 	ExpectWithOffset(1, err).NotTo(HaveOccurred(), "Failed to build the manager(Operator) image")
 
+	By("building the webhook cert job image")
+	cmd = exec.Command("make", "docker-build-webhookcertjob",
+		"CONTAINER_TOOL=podman",
+		fmt.Sprintf("WEBHOOKCERTJOB_IMG=%s", webhookCertJobImage))
+	_, err = utils.Run(cmd)
+	ExpectWithOffset(1, err).NotTo(HaveOccurred(), "Failed to build the webhook cert job image")
+
 	// TODO(user): If you want to change the e2e test vendor from Kind, ensure the image is
 	// built and available before running the tests. Also, remove the following block.
 	By("loading the manager(Operator) image on Kind")
 	err = utils.LoadImageToKindClusterWithName(projectImage)
 	ExpectWithOffset(1, err).NotTo(HaveOccurred(), "Failed to load the manager(Operator) image into Kind")
+
+	By("loading the webhook cert job image on Kind")
+	err = utils.LoadImageToKindClusterWithName(webhookCertJobImage)
+	ExpectWithOffset(1, err).NotTo(HaveOccurred(), "Failed to load the webhook cert job image into Kind")
 
 	// The tests-e2e are intended to run on a temporary cluster that is created and destroyed for testing.
 	// To prevent errors when tests run in environments with CertManager already installed,
